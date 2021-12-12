799d9c690cf8672ea36a9e6a6c2558cdc610b267
Atharva Sen <89924185+AtharvaSenBarai@users.noreply.github.com>
2021-12-12 17:03:45 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyApQhIWMO8zNFnstSFsHnB0T9-GzhkMha0",
+            authDomain: "https://atharvasenbarai.github.io/",
+            databaseURL: "https://website-80fc1-default-rtdb.firebaseio.com",
+            projectId: "website-80fc1",
+            storageBucket: "website-80fc1.appspot.com",
+            messagingSenderId: "712562391435",
+            appId: "1:712562391435:web:bbc8c6affd6d766fe4178e",
+            measurementId: "G-CTYYF9FEHR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
